20103590b8a891cbc6d096717ea77e5d94a88be7
Александр Косолапов <kosolapov1976@gmail.com>
2024-01-11 11:31:14 +0600

    feat: add left aside

--- resources/js/Components/NavLink.vue
+++ resources/js/Components/NavLink.vue
@@ -3,24 +3,25 @@ import { computed } from 'vue'
 import { Link } from '@inertiajs/vue3'
 
 const props = defineProps({
-  href: {
-    type: String,
-    required: true
-  },
-  active: {
-    type: Boolean
-  }
+    href: {
+        type: String,
+        required: true
+    },
+    active: {
+        type: Boolean
+    }
 })
 
-const classes = computed(() =>
-  props.active
-    ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-    : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
-)
+const lineStyle = computed(() => {
+    return {
+        borderLeft: props.active ? '2px solid red' : 'none', // Замените 'red' на нужный цвет
+        paddingLeft: props.active ? '8px' : '0',
+    };
+});
 </script>
 
 <template>
-  <Link :href="href" :class="classes">
-    <slot />
-  </Link>
+    <Link :href="href" :class="{ 'inline-flex items-center w-full text-sm font-semibold text-gray-800 dark:text-gray-100 relative': active }">
+        <span class="flex ml-4" :style="lineStyle"><slot></slot></span>
+    </Link>
 </template>
---
+++ resources/js/Layouts/TopMenu.vue
@@ -0,0 +1,57 @@
+<template>
+    <header class="z-10 py-4 bg-white shadow-md">
+      <div class="container flex justify-between items-center px-6 mx-auto h-full text-purple-600 md:justify-end">
+        <!-- Mobile hamburger -->
+        <button @click="$page.props.showingMobileMenu = !$page.props.showingMobileMenu" class="p-1 mr-5 -ml-1 rounded-md md:hidden focus:outline-none focus:shadow-outline-purple" aria-label="Menu">
+          <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+            <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
+          </svg>
+        </button>
+
+          <div class="sm:flex sm:items-center sm:ms-6">
+              <!-- Settings Dropdown -->
+              <div class="ms-3 relative">
+                  <Dropdown align="right" width="48">
+                      <template #trigger>
+                    <span class="inline-flex rounded-md">
+                      <button
+                          type="button"
+                          class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                      >
+                        {{ $page.props.auth.user.name }}
+
+                        <svg
+                            class="ms-2 -me-0.5 h-4 w-4"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                        >
+                          <path
+                              fill-rule="evenodd"
+                              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                              clip-rule="evenodd"
+                          />
+                        </svg>
+                      </button>
+                    </span>
+                      </template>
+
+                      <template #content>
+                          <DropdownLink :href="route('profile.edit')"> Profile</DropdownLink>
+                          <DropdownLink :href="route('logout')" method="post" as="button">
+                              Log Out
+                          </DropdownLink>
+                      </template>
+                  </Dropdown>
+              </div>
+          </div>
+
+
+      </div>
+    </header>
+</template>
+
+<script setup>
+import Dropdown from '@/Components/Dropdown.vue'
+import DropdownLink from '@/Components/DropdownLink.vue'
+</script>
